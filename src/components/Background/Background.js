c5c4800645e1d778c6d608ebaf917ad5002a54e6
Brandt Collins <seebrandt@gmail.com>
2020-09-08 11:57:32 -0400

    CSS edits

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "./Background.module.scss";
 
-const Background = () => {
-  return <div className={styles.background}></div>;
+const classNames = require("classnames");
+
+const Background = (props) => {
+  const foodClass = props.category ? null : styles.hide;
+  const drinkClass = !props.category ? null : styles.hide;
+
+  return (
+    <>
+      <div className={classNames(styles.food, foodClass)}></div>
+      <div className={classNames(styles.drinks, drinkClass)}></div>
+    </>
+  );
 };
 
 export default Background;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,39 @@
 import React from "react";
 import styles from "./Header.module.scss";
 
-const Header = () => {
+const classNames = require("classnames");
+
+const Header = (props) => {
+  const { category, handleCategoryChange } = props;
+
+  const toggle = !category ? null : styles.active;
+
+  const handleToggle = () => {
+    handleCategoryChange(!category);
+  };
+
   return (
-    <div className={styles.Header}>
-      <h1 className={styles.logo}>
-        <a href="/">feedme</a>
-      </h1>
-    </div>
+    <>
+      <div className={styles.Header}>
+        <h1 className={styles.logo}>
+          <a href="/">feedme</a>
+        </h1>
+        <div onClick={handleToggle} className={styles.themeSwitch}>
+          <div className={styles.lable}>
+            <h5>Food</h5>
+          </div>
+          <div className={styles.lable}>
+            <h5>Drinks</h5>
+          </div>
+          <div className={classNames(styles.switch, toggle)}></div>
+        </div>
+      </div>
+      <div className={styles.hero}>
+        <h2>“Where do you want to eat?”</h2>
+        <h2>“IDK, where do you want to eat?”</h2>
+        <p>We got you covered, start your search below.</p>
+      </div>
+    </>
   );
 };
 
